6e72b9b758798d67186a0502235b05f98fa14a3d
Rui Caridade <ruidanielcaridade@gmail.com>
2024-03-19 23:29:14 +0000

    feat: add telescope, mason, etc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,13 +1,7 @@
----@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "catppuccin",
 }
 
 return M
