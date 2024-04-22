e29258949b08a7fb54025cf12603ba576087e897
Rui Caridade <rui@lineupnow.com>
2024-04-22 11:01:02 +0100

    feat: add telescope mappings to find symbols

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,9 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 local telescope = require "telescope.builtin"
-map("n", "<leader>fs", telescope.lsp_dynamic_workspace_symbols)
+map("n", "<leader>gr", telescope.lsp_references, { desc = "Telescope Goto References" })
+map("n", "<leader>fd", telescope.lsp_document_symbols, { desc = "Telescope Document Symbols" })
+map("n", "<leader>fs", telescope.lsp_dynamic_workspace_symbols, { desc = "Telescope Workspace Symbols" })
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
