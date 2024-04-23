0553644bff2836db6efb835b4aad84bed5ed33af
Rui Caridade <rui@lineupnow.com>
2024-04-23 17:04:34 +0100

    feat: prefer using local mypy

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -26,7 +26,16 @@ require("mason-null-ls").setup {
     completion = true,
     hover = true,
   },
-  handlers = {},
+  handlers = {
+    mypy = function(source_name, methods)
+      null_ls.register(null_ls.builtins.diagnostics.mypy.with {
+        extra_args = function()
+          local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
+          return { "--python-executable", virtual .. "/bin/python3" }
+        end,
+      })
+    end,
+  },
 }
 
 null_ls.setup()
