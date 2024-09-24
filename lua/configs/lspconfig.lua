b6764da4d7d9277aafc098a1c1b1db9ad4d83bd1
Rui Caridade <rui@lineupnow.com>
2024-09-24 16:05:56 +0100

    fix: issue with diagnostics not showing in new nvchad version

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -59,6 +59,26 @@ for name, opts in pairs(servers) do
   opts.on_init = on_init
   opts.on_attach = on_attach
   opts.capabilities = capabilities
+  opts.settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
+          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/luv/library",
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  }
 
+  dofile(vim.g.base46_cache .. "lsp")
+  require("nvchad.lsp").diagnostic_config()
   require("lspconfig")[name].setup(opts)
 end
