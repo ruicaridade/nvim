1eb81ce39dccc63dbabde5840239dee3081a8b97
Rui Caridade <ruidanielcaridade@gmail.com>
2025-12-14 15:54:13 +0000

    add the minimal requirements

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.o.swapfile = false
 vim.g.mapleader = ' '
 vim.o.winborder = "rounded"
 vim.o.cmdheight = 1
+vim.o.autoread = true
 
 -- Text
 vim.o.number = true
@@ -22,7 +23,7 @@ vim.pack.add({
   -- Theme
   { src = 'https://github.com/rose-pine/neovim' },
 
-  -- Utilities
+  -- Misc & utilities
   { src = 'https://github.com/nvim-mini/mini.nvim' },
   { src = 'https://github.com/folke/snacks.nvim' },
 
@@ -33,9 +34,23 @@ vim.pack.add({
   { src = 'https://github.com/neovim/nvim-lspconfig' },
   { src = 'https://github.com/mason-org/mason.nvim' },
   { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+
+  -- AI
+  { src = 'https://github.com/NickvanDyke/opencode.nvim' },
+
+  -- Cmp
+  { src = 'https://github.com/saghen/blink.cmp' },
+
+  -- Git
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
 })
 
 -- Theme
+require('rose-pine').setup({
+  styles = {
+    transparency = true,
+  }
+})
 vim.cmd('colorscheme rose-pine')
 
 -- Mini
@@ -48,20 +63,46 @@ require('mini.cmdline').setup({
 require('mini.pairs').setup()
 require('mini.icons').setup()
 require('mini.snippets').setup()
-require('mini.completion').setup()
 require('mini.icons').setup()
 
 -- Snacks
 require('snacks').setup({
-  explorer = {
-    enabled = true
+  input = { enabled = true },
+  picker = { enabled = true },
+  statuscolumn = { enabled = true },
+  indent = {
+    enabled = true,
+    animate = {
+      enabled = false
+    },
+    scope = {
+      enabled = false
+    }
   }
 })
 
 -- LSP
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = { 'lua_ls' }
+  ensure_installed = { 'lua_ls', 'pyright', 'ts_ls', 'rust_analyzer' }
+})
+
+-- Cmp
+require('blink.cmp').setup({
+  keymap = {
+    preset = 'enter'
+  },
+  appearance = {
+    nerd_font_variant = 'normal'
+  },
+  fuzzy = {
+    implementation = 'lua'
+  }
+})
+
+-- Git
+require('gitsigns').setup({
+  current_line_blame = true,
 })
 
 -- Keybinds: LSP
@@ -69,17 +110,31 @@ vim.keymap.set('n', '<leader>lm', ':Mason<CR>')
 vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format)
 vim.keymap.set('n', 'gd', function() Snacks.picker.lsp_definitions() end, { desc = 'Goto Definition' })
 vim.keymap.set('n', 'gD', function() Snacks.picker.lsp_declarations() end, { desc = 'Goto Declaration' })
-vim.keymap.set('n', 'gr', function() Snacks.picker.lsp_references() end, { nowait= true, desc = 'References' })
+vim.keymap.set('n', 'gr', function() Snacks.picker.lsp_references() end, { nowait = true, desc = 'References' })
 
 -- Keybinds: Find
 vim.keymap.set('n', '<leader>sf', function() Snacks.picker.files() end, { desc = 'Search files' })
 vim.keymap.set('n', '<leader>sg', function() Snacks.picker.grep() end, { desc = 'Search Grep' })
 vim.keymap.set('n', '<leader>sb', function() Snacks.picker.buffers() end, { desc = 'Search Buffers' })
 vim.keymap.set('n', '<leader>ss', function() Snacks.picker.lsp_symbols() end, { desc = 'Search Symbols' })
-vim.keymap.set('n', '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, { desc = 'Search Workspace Symbols' })
+vim.keymap.set('n', '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end,
+  { desc = 'Search Workspace Symbols' })
 
 -- Keybinds: File explorer
 vim.keymap.set('n', '<leader>e', function() Snacks.explorer() end)
 
 -- Keybinds: Git
 vim.keymap.set('n', '<leader>gg', function() Snacks.lazygit() end)
+vim.keymap.set('n', '<leader>gb', ':Gitsigns blame<CR>')
+
+-- Keybinds: AI
+local openc = require('opencode')
+vim.keymap.set({ "n", "x" }, "<leader>aa", function() openc.ask("@this: ", { submit = true }) end,
+  { desc = "Ask opencode" })
+vim.keymap.set({ "n", "x" }, "<leader>ae", function() openc.select() end, { desc = "Execute opencode action…" })
+vim.keymap.set({ "n", "x" }, "<leader>al", function() openc.prompt("@this") end, { desc = "Add to opencode" })
+vim.keymap.set({ "n", "t" }, "<leader>at", function() openc.toggle() end, { desc = "Toggle opencode" })
+vim.keymap.set("n", "<S-C-u>", function() openc.command("session.half.page.up") end, { desc = "opencode page up" })
+vim.keymap.set("n", "<S-C-d>", function() openc.command("session.half.page.down") end, { desc = "opencode page down" })
+vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment', noremap = true })
+vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement', noremap = true })
