068e4ecad2e08096d4b3900a6a7dd8dcdeaceb76
Rui Caridade <ruidanielcaridade@gmail.com>
2025-12-14 13:39:51 +0000

    add file explorer and keybinds

--- init.lua
+++ init.lua
@@ -1,39 +1,85 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
+-- Misc
+vim.o.swapfile = false
+vim.g.mapleader = ' '
+vim.o.winborder = "rounded"
+vim.o.cmdheight = 1
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+-- Text
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
 
-if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
+-- Tab
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.smartindent = true
+vim.o.shiftround = true
 
-vim.opt.rtp:prepend(lazypath)
+-- Plugins
+vim.pack.add({
+  -- Theme
+  { src = 'https://github.com/rose-pine/neovim' },
 
-local lazy_config = require "configs.lazy"
+  -- Utilities
+  { src = 'https://github.com/nvim-mini/mini.nvim' },
+  { src = 'https://github.com/folke/snacks.nvim' },
 
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-  },
+  -- Automatically set indentation based on file
+  { src = 'https://github.com/tpope/vim-sleuth' },
 
-  { import = "plugins" },
-}, lazy_config)
+  -- LSP
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/mason-org/mason.nvim' },
+  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+})
 
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
+-- Theme
+vim.cmd('colorscheme rose-pine')
 
-require "nvchad.autocmds"
+-- Mini
+require('mini.statusline').setup()
+require('mini.cmdline').setup({
+  autocomplete = {
+    delay = 250
+  }
+})
+require('mini.pairs').setup()
+require('mini.icons').setup()
+require('mini.snippets').setup()
+require('mini.completion').setup()
+require('mini.icons').setup()
 
-vim.schedule(function()
-  require "mappings"
-end)
+-- Snacks
+require('snacks').setup({
+  explorer = {
+    enabled = true
+  }
+})
+
+-- LSP
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { 'lua_ls' }
+})
+
+-- Keybinds: LSP
+vim.keymap.set('n', '<leader>lm', ':Mason<CR>')
+vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format)
+vim.keymap.set('n', 'gd', function() Snacks.picker.lsp_definitions() end, { desc = 'Goto Definition' })
+vim.keymap.set('n', 'gD', function() Snacks.picker.lsp_declarations() end, { desc = 'Goto Declaration' })
+vim.keymap.set('n', 'gr', function() Snacks.picker.lsp_references() end, { nowait= true, desc = 'References' })
+
+-- Keybinds: Find
+vim.keymap.set('n', '<leader>sf', function() Snacks.picker.files() end, { desc = 'Search files' })
+vim.keymap.set('n', '<leader>sg', function() Snacks.picker.grep() end, { desc = 'Search Grep' })
+vim.keymap.set('n', '<leader>sb', function() Snacks.picker.buffers() end, { desc = 'Search Buffers' })
+vim.keymap.set('n', '<leader>ss', function() Snacks.picker.lsp_symbols() end, { desc = 'Search Symbols' })
+vim.keymap.set('n', '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, { desc = 'Search Workspace Symbols' })
+
+-- Keybinds: File explorer
+vim.keymap.set('n', '<leader>e', function() Snacks.explorer() end)
+
+-- Keybinds: Git
+vim.keymap.set('n', '<leader>gg', function() Snacks.lazygit() end)
