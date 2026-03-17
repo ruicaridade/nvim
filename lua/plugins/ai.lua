14c46db376a5708cc5f57922d17c959f0c49ee6b
Rui Caridade <ruidanielcaridade@gmail.com>
2026-03-17 17:50:06 +0000

    remove supermaven

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -53,23 +53,4 @@ return {
       },
     },
   },
-  {
-    "supermaven-inc/supermaven-nvim",
-    opts = {
-      keymaps = {
-        accept_suggestion = "<Tab>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
-      },
-      log_level = "info",
-      disable_inline_completion = false,
-      disable_keymaps = false,
-      diff_opts = {
-        auto_close_on_accept = true,
-        vertical_split = true,
-        open_in_current_tab = true,
-        keep_terminal_focus = false,
-      },
-    }
-  },
 }
